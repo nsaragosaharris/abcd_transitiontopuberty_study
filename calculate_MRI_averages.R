fa98284d19727c759e015fc2354a8a92c29c3a82
Natalie Saragosa-Harris <nsaragosaharris@ucla.edu>
2021-11-15 15:51:03 -0800

    Add files via upload

--- calculate_MRI_averages.R
+++ calculate_MRI_averages.R
@@ -10,10 +10,10 @@ data_dir = ((dirname(here())))
 
 data_folder <- file.path(data_dir,"ABCD","derivatives")
 
-file_name <- "nda20.csv" # specify file name here. 
+file_name <- "nda30.csv" # specify file name here. 
 
-nda20 <- read.csv(file.path(data_folder,file_name))
-nrow(nda20) # 54599.
+nda30 <- read.csv(file.path(data_folder,file_name))
+nrow(nda30) # 54599.
 
 MRI_columns <- c("tfmri_ma_acdn_b_scs_aarh", # Accumbens reward vs. neutral anticipation.
 "tfmri_ma_acdn_b_scs_aalh",
@@ -36,50 +36,50 @@ MRI_columns <- c("tfmri_ma_acdn_b_scs_aarh", # Accumbens reward vs. neutral anti
 "tfmri_ma_rpvnfb_b_cds_lobofrrh", # Lateral OFC reward positive versus negative feedback.
 "tfmri_ma_rpvnfb_b_cds_lobofrlh")
 
-nda20[MRI_columns] <- lapply(nda20[MRI_columns], as.numeric)  # Make sure that they are numeric (they are read in as characters).
+nda30[MRI_columns] <- lapply(nda30[MRI_columns], as.numeric)  # Make sure that they are numeric (they are read in as characters).
 
 
 # Average right and left hemispheres.
 # Note: If either is "NA", then the whole thing will be "NA".
-nda20$accumbens_rvsn_ant <- (nda20$tfmri_ma_acdn_b_scs_aarh + nda20$tfmri_ma_acdn_b_scs_aalh)/2
-nda20$caudate_rvsn_ant <- (nda20$tfmri_ma_acdn_b_scs_cdrh + nda20$tfmri_ma_acdn_b_scs_cdlh)/2
-nda20$putamen_rvsn_ant <- (nda20$tfmri_ma_acdn_b_scs_ptrh + nda20$tfmri_ma_acdn_b_scs_ptlh)/2
-nda20$mOFC_rvsn_ant <- (nda20$tfmri_ma_arvn_b_cds_mobofrrh + nda20$tfmri_ma_arvn_b_cds_mobofrlh)/2
-nda20$lOFC_rvsn_ant <- (nda20$tfmri_ma_arvn_b_cds_lobofrrh + nda20$tfmri_ma_arvn_b_cds_lobofrlh)/2
+nda30$accumbens_rvsn_ant <- (nda30$tfmri_ma_acdn_b_scs_aarh + nda30$tfmri_ma_acdn_b_scs_aalh)/2
+nda30$caudate_rvsn_ant <- (nda30$tfmri_ma_acdn_b_scs_cdrh + nda30$tfmri_ma_acdn_b_scs_cdlh)/2
+nda30$putamen_rvsn_ant <- (nda30$tfmri_ma_acdn_b_scs_ptrh + nda30$tfmri_ma_acdn_b_scs_ptlh)/2
+nda30$mOFC_rvsn_ant <- (nda30$tfmri_ma_arvn_b_cds_mobofrrh + nda30$tfmri_ma_arvn_b_cds_mobofrlh)/2
+nda30$lOFC_rvsn_ant <- (nda30$tfmri_ma_arvn_b_cds_lobofrrh + nda30$tfmri_ma_arvn_b_cds_lobofrlh)/2
 
 # Do the same for feedback stage as well.
-nda20$accumbens_posvsneg_feedback <- (nda20$tfmri_ma_rpvnfb_b_scs_aarh + nda20$tfmri_ma_rpvnfb_b_scs_aalh)/2
-nda20$caudate_posvsneg_feedback <- (nda20$tfmri_ma_rpvnfb_b_scs_cdrh + nda20$tfmri_ma_rpvnfb_b_scs_cdlh)/2
-nda20$putamen_posvsneg_feedback <- (nda20$tfmri_ma_rpvnfb_b_scs_ptrh + nda20$tfmri_ma_rpvnfb_b_scs_ptlh)/2
-nda20$mOFC_posvsneg_feedback <- (nda20$tfmri_ma_rpvnfb_b_cds_mobofrrh + nda20$tfmri_ma_rpvnfb_b_cds_mobofrlh)/2
-nda20$lOFC_posvsneg_feedback <- (nda20$tfmri_ma_rpvnfb_b_cds_lobofrrh + nda20$tfmri_ma_rpvnfb_b_cds_lobofrlh)/2
+nda30$accumbens_posvsneg_feedback <- (nda30$tfmri_ma_rpvnfb_b_scs_aarh + nda30$tfmri_ma_rpvnfb_b_scs_aalh)/2
+nda30$caudate_posvsneg_feedback <- (nda30$tfmri_ma_rpvnfb_b_scs_cdrh + nda30$tfmri_ma_rpvnfb_b_scs_cdlh)/2
+nda30$putamen_posvsneg_feedback <- (nda30$tfmri_ma_rpvnfb_b_scs_ptrh + nda30$tfmri_ma_rpvnfb_b_scs_ptlh)/2
+nda30$mOFC_posvsneg_feedback <- (nda30$tfmri_ma_rpvnfb_b_cds_mobofrrh + nda30$tfmri_ma_rpvnfb_b_cds_mobofrlh)/2
+nda30$lOFC_posvsneg_feedback <- (nda30$tfmri_ma_rpvnfb_b_cds_lobofrrh + nda30$tfmri_ma_rpvnfb_b_cds_lobofrlh)/2
 
 # striatum_rvsn_ant = reward vs. neutral anticipation.
-nda20$striatum_rvsn_ant <- (nda20$accumbens_rvsn_ant + 
-                             nda20$caudate_rvsn_ant+
-                             nda20$putamen_rvsn_ant)/3
+nda30$striatum_rvsn_ant <- (nda30$accumbens_rvsn_ant + 
+                             nda30$caudate_rvsn_ant+
+                             nda30$putamen_rvsn_ant)/3
 
 # striatum_posvsneg_feedback = reward positive vs. negative feedback.
-nda20$striatum_posvsneg_feedback <- (nda20$accumbens_posvsneg_feedback +
-                                      nda20$caudate_posvsneg_feedback +
-                                      nda20$putamen_posvsneg_feedback)/3
+nda30$striatum_posvsneg_feedback <- (nda30$accumbens_posvsneg_feedback +
+                                      nda30$caudate_posvsneg_feedback +
+                                      nda30$putamen_posvsneg_feedback)/3
 
 # Calculate z scores for all of these values.
-nda20$accumbens_rvsn_ant_z <- scale(nda20$accumbens_rvsn_ant)
-nda20$caudate_rvsn_ant_z <- scale(nda20$caudate_rvsn_ant)
-nda20$putamen_rvsn_ant_z <- scale(nda20$putamen_rvsn_ant)
-nda20$mOFC_rvsn_ant_z <- scale(nda20$mOFC_rvsn_ant)
-nda20$lOFC_rvsn_ant_z <- scale(nda20$lOFC_rvsn_ant)
+nda30$accumbens_rvsn_ant_z <- scale(nda30$accumbens_rvsn_ant)
+nda30$caudate_rvsn_ant_z <- scale(nda30$caudate_rvsn_ant)
+nda30$putamen_rvsn_ant_z <- scale(nda30$putamen_rvsn_ant)
+nda30$mOFC_rvsn_ant_z <- scale(nda30$mOFC_rvsn_ant)
+nda30$lOFC_rvsn_ant_z <- scale(nda30$lOFC_rvsn_ant)
 
-nda20$accumbens_posvsneg_feedback_z <- scale(nda20$accumbens_posvsneg_feedback)
-nda20$caudate_posvsneg_feedback_z <- scale(nda20$caudate_posvsneg_feedback)
-nda20$putamen_posvsneg_feedback_z <- scale(nda20$putamen_posvsneg_feedback)
-nda20$mOFC_posvsneg_feedback_z <- scale(nda20$mOFC_posvsneg_feedback)
-nda20$lOFC_posvsneg_feedback_z <- scale(nda20$lOFC_posvsneg_feedback)
+nda30$accumbens_posvsneg_feedback_z <- scale(nda30$accumbens_posvsneg_feedback)
+nda30$caudate_posvsneg_feedback_z <- scale(nda30$caudate_posvsneg_feedback)
+nda30$putamen_posvsneg_feedback_z <- scale(nda30$putamen_posvsneg_feedback)
+nda30$mOFC_posvsneg_feedback_z <- scale(nda30$mOFC_posvsneg_feedback)
+nda30$lOFC_posvsneg_feedback_z <- scale(nda30$lOFC_posvsneg_feedback)
 
-nda20$striatum_rvsn_ant_z <- scale(nda20$striatum_rvsn_ant)
-nda20$striatum_posvsneg_feedback_z <- scale(nda20$striatum_posvsneg_feedback)
+nda30$striatum_rvsn_ant_z <- scale(nda30$striatum_rvsn_ant)
+nda30$striatum_posvsneg_feedback_z <- scale(nda30$striatum_posvsneg_feedback)
 
 setwd(data_folder)
-saveRDS(nda20, 'nda20.rds')
-write.csv(nda20,'nda20.csv', row.names = FALSE)
+saveRDS(nda30, 'nda30.rds')
+write.csv(nda30,'nda30.csv', row.names = FALSE)
--- rename_alias_variables.R
+++ rename_alias_variables.R
@@ -17,7 +17,7 @@ nda20 <- read.csv('nda20.csv')
 variables <- as.data.frame(variablenames$Variable.Name)
 variables <- variables[!apply(variables == "", 1, all),] # Remove empty rows.
 variables <- tolower(variables)  # Make sure they are all lowercase.
-missingvariables <- setdiff(variables,colnames(nda20)) # List variables that are in the "variable names" file but not in the data file.
+missingvariables <- setdiff(variables,colnames(nda30)) # List variables that are in the "variable names" file but not in the data file.
 missingvariables <- as.data.frame(missingvariables)
 colnames(missingvariables) <- "Variable.Name"
 
@@ -45,11 +45,11 @@ print(no_aliases$Variable.Name)
 missingvariables <- subset(missingvariables, Aliases!= "")
 
 # Do these aliases exist in the dataframe?
-aliases_in_data <- intersect(missingvariables$Aliases,colnames(nda20))
+aliases_in_data <- intersect(missingvariables$Aliases,colnames(nda30))
 print("Alias exists in dataframe for the following variables: ")
 print(aliases_in_data)
 
-aliases_not_in_data <- setdiff(missingvariables$Aliases,colnames(nda20))
+aliases_not_in_data <- setdiff(missingvariables$Aliases,colnames(nda30))
 print("Neither the original variable name nor its alias exist in the dataframe for the following variables: ")
 print(aliases_not_in_data)
 # These variables need to be calculated so they are not in the dataframe currently.
@@ -66,11 +66,11 @@ for (i in 1:length(aliases_in_data)){
   new_name <- subset(aliases, Aliases == alias)
   new_name <- new_name$Variable.Name
   # Find alias in dataframe and replace with the new name.
-  names(nda20)[names(nda20) == alias] <- new_name
+  names(nda30)[names(nda30) == alias] <- new_name
 }
 
 print("The following variables were not found in the data frame under this name or an alias: ")
-print(setdiff(variables,colnames(nda20)))
+print(setdiff(variables,colnames(nda30)))
 
 # The bisbas ones need to be calculated (see definition in variable definitions file).
 # bisbas_ss_basm_rr.
@@ -81,11 +81,11 @@ setwd(outputdirectory)
 
 # As a last step we can save the data in R's native file format.
 
-saveRDS(nda20, glue('{outputdirectory}/nda20.rds'))
+saveRDS(nda30, glue('{outputdirectory}/nda30.rds'))
 
-names.nda20=colnames(nda20)
+names.nda30=colnames(nda30)
 
-save(file="names.nda20.RData",names.nda20)
+save(file="names.nda30.RData",names.nda30)
 
 # Save as .csv file as well.
-write.csv(nda20, glue('{outputdirectory}/nda20.csv', row.names = FALSE))
+write.csv(nda30, glue('{outputdirectory}/nda30.csv', row.names = FALSE))
