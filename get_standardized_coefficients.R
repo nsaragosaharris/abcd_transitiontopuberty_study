da0b5946e6b7c821da650231ee095836e599992b
Natalie Saragosa-Harris <nsaragosaharris@ucla.edu>
2022-08-29 11:47:29 -0700

    Add files via upload

---
+++ get_standardized_coefficients.R
@@ -0,0 +1,8 @@
+standardized_coefficient <- function(object) {
+  sdy <- sd(getME(object,"y"))
+  sdx <- apply(getME(object,"X"), 2, sd)
+  sc <- fixef(object)*sdx/sdy
+  se.fixef <- coef(summary(object))[,"Std. Error"]
+  se <- se.fixef*sdx/sdy
+  return(data.frame(stdcoef=sc, stdse=se))
+}
